540f9088ec042dfebb1b1587fea491ac9d71c09b
kparasch <konstantinos.paraschou@cern.ch>
2019-11-12 15:47:04 +0100

    silenced file manager slightly

--- kostas_filemanager.py
+++ kostas_filemanager.py
@@ -22,7 +22,7 @@ def h5_to_dict(filename, group=None):
 def h5_to_obj(filename, group=None):
     return obj_from_dict(h5_to_dict(filename, group=group))
 
-def dict_to_h5(dict_save, filename, compression_opts=4, group=None, readwrite_opts='w'):
+def dict_to_h5(dict_save, filename, compression_opts=4, group=None, readwrite_opts='w', verbose=False):
     with h5py.File(filename, readwrite_opts) as fid:
 
         if group == None :
@@ -31,8 +31,8 @@ def dict_to_h5(dict_save, filename, compression_opts=4, group=None, readwrite_op
             grp = fid.create_group(group)
 
         for kk in dict_save.keys():
+            if verbose: print('Writing '+kk)
             if isinstance(dict_save[kk], numpy.ndarray):
-                print('Compressing '+kk)
                 dset = grp.create_dataset(kk, shape=dict_save[kk].shape, dtype=dict_save[kk].dtype, compression='gzip', compression_opts=compression_opts)
                 dset[...] = dict_save[kk]
             else:
