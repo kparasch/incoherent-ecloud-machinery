844aa9596535ac6159e547ae33e29a3740e38dca
Konstantinos Paraschou <kparasch@cern.ch>
2020-05-16 18:49:25 +0200

    update processing script

--- MonteCarlo_analysis/process_collision_files_noec.py
+++ MonteCarlo_analysis/process_collision_files_noec.py
@@ -1,36 +1,38 @@
-import myfilemanager_sixtracklib as mfm
+import sys
+sys.path.append('../Tools')
+import kostas_filemanager as kfm
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-in_folder = '/eos/user/k/kparasch/Decohered_STL_data/'
-out_folder = '/eos/user/k/kparasch/processed_Decohered_STL_data/'
-filelist10 = ['losses_sixtracklib_4271153.0.h5']
+in_folder = '/home/kparasch/Storage/Decohered_STL_data/'
+out_folder = '/home/kparasch/Storage/processed_Decohered_STL_data/'
+filelist10 = ['losses_sixtracklib.4271153.0.h5']
 for ii in range(2,12):
-     filelist10.append(f'losses_sixtracklib_4271153.{ii}.h5')
+     filelist10.append(f'losses_sixtracklib.4271153.{ii}.h5')
 filelist20 = []
 for ii in range(0,6):
-    filelist20.append(f'losses_sixtracklib_4295944.{ii}.h5')
+    filelist20.append(f'losses_sixtracklib.4295944.{ii}.h5')
 for ii in range(0,11):
-    filelist20.append(f'losses_sixtracklib_4328392.{ii}.h5')
+    filelist20.append(f'losses_sixtracklib.4328392.{ii}.h5')
 for ii in range(0,12):
-    filelist20.append(f'losses_sixtracklib_4591293.{ii}.h5')
+    filelist20.append(f'losses_sixtracklib.4591293.{ii}.h5')
 filelist15 = []
-filelist15.append(f'losses_sixtracklib_4351523.0.h5')
-filelist15.append(f'losses_sixtracklib_4351523.1.h5')
-filelist15.append(f'losses_sixtracklib_4351523.2.h5')
-filelist15.append(f'losses_sixtracklib_4351523.4.h5')
-filelist15.append(f'losses_sixtracklib_4351523.5.h5')
+filelist15.append(f'losses_sixtracklib.4351523.0.h5')
+filelist15.append(f'losses_sixtracklib.4351523.1.h5')
+filelist15.append(f'losses_sixtracklib.4351523.2.h5')
+filelist15.append(f'losses_sixtracklib.4351523.4.h5')
+filelist15.append(f'losses_sixtracklib.4351523.5.h5')
 for ii in range(0,12):
-    filelist15.append(f'losses_sixtracklib_4395210.{ii}.h5')
+    filelist15.append(f'losses_sixtracklib.4395210.{ii}.h5')
 filelist05 = []
 for ii in range(0,12):
-    filelist05.append(f'losses_sixtracklib_4448724.{ii}.h5')
+    filelist05.append(f'losses_sixtracklib.4448724.{ii}.h5')
 filelist00 = []
 for ii in range(0,8):
-    filelist00.append(f'losses_sixtracklib_4487357.{ii}.h5')
+    filelist00.append(f'losses_sixtracklib.4487357.{ii}.h5')
 for ii in range(9,12):
-    filelist00.append(f'losses_sixtracklib_4487357.{ii}.h5')
+    filelist00.append(f'losses_sixtracklib.4487357.{ii}.h5')
 
 
 filelists = [filelist00, filelist05, filelist10, filelist15, filelist20] 
@@ -38,10 +40,10 @@ fnames = ['collision_ptau0.0e-4', 'collision_ptau0.5e-4', 'collision_ptau1.0e-4'
 
 def get_ob_input(myfile):
     print(myfile)
-    #out_data       = mfm.h5_to_dict(myfile, group = 'output')
-    in_data        = mfm.h5_to_dict(myfile, group = 'input')
-    optics         = mfm.h5_to_dict(myfile, group = 'beam-optics')
-    particle_on_CO = mfm.h5_to_dict(myfile, group = 'closed-orbit')
+    #out_data       = kfm.h5_to_dict(myfile, group = 'output')
+    in_data        = kfm.h5_to_dict(myfile, group = 'input')
+    optics         = kfm.h5_to_dict(myfile, group = 'beam-optics')
+    particle_on_CO = kfm.h5_to_dict(myfile, group = 'closed-orbit')
        
     
     e1 = optics['epsn_x'] / (optics['beta0'] * optics['gamma0'])
@@ -85,9 +87,9 @@ def get_ob_input(myfile):
 
 def get_ob_first(myfile):
     print(myfile)
-    out_data       = mfm.h5_to_dict(myfile, group = 'output')
-    optics         = mfm.h5_to_dict(myfile, group = 'beam-optics')
-    particle_on_CO = mfm.h5_to_dict(myfile, group = 'closed-orbit')
+    out_data       = kfm.h5_to_dict(myfile, group = 'output')
+    optics         = kfm.h5_to_dict(myfile, group = 'beam-optics')
+    particle_on_CO = kfm.h5_to_dict(myfile, group = 'closed-orbit')
        
     
     e1 = optics['epsn_x'] / (optics['beta0'] * optics['gamma0'])
@@ -132,9 +134,9 @@ def get_ob_first(myfile):
 
 def get_ob_skip(myfile):
     print(myfile)
-    out_data       = mfm.h5_to_dict(myfile, group = 'output')
-    optics         = mfm.h5_to_dict(myfile, group = 'beam-optics')
-    particle_on_CO = mfm.h5_to_dict(myfile, group = 'closed-orbit')
+    out_data       = kfm.h5_to_dict(myfile, group = 'output')
+    optics         = kfm.h5_to_dict(myfile, group = 'beam-optics')
+    particle_on_CO = kfm.h5_to_dict(myfile, group = 'closed-orbit')
        
     
     e1 = optics['epsn_x'] / (optics['beta0'] * optics['gamma0'])
@@ -178,9 +180,9 @@ def get_ob_skip(myfile):
 
 def get_ob_last(myfile):
     print(myfile)
-    out_data       = mfm.h5_to_dict(myfile, group = 'output')
-    optics         = mfm.h5_to_dict(myfile, group = 'beam-optics')
-    particle_on_CO = mfm.h5_to_dict(myfile, group = 'closed-orbit')
+    out_data       = kfm.h5_to_dict(myfile, group = 'output')
+    optics         = kfm.h5_to_dict(myfile, group = 'beam-optics')
+    particle_on_CO = kfm.h5_to_dict(myfile, group = 'closed-orbit')
        
     
     e1 = optics['epsn_x'] / (optics['beta0'] * optics['gamma0'])
@@ -224,7 +226,6 @@ def get_ob_last(myfile):
     return ob
 
 def merge_obs(obs):
-    ob_master = o
     
     J1 = obs[0]['J1']
     J2 = obs[0]['J2']
@@ -240,30 +241,32 @@ def merge_obs(obs):
         tau   = np.hstack((tau  , obs[i]['tau']))
         turn  = np.hstack((turn , obs[i]['turn']))
 
-    ob = { 'J1'   : J1,
+    ob_master = { 'J1'   : J1,
            'J2'   : J2,
            'phi1' : phi1,
            'phi2' : phi2,
            'tau'  : zeta,
            'turn' : turn
          }
-    return ob
+    return ob_master
 
 
-filelist = filelists[0]
-suffix = '_first.h5'
-fname = fnames[0]+suffix
-print(fname)
-outfile = out_folder + fname
-obs = []
-for myfile in filelist:
-    obs.append( get_ob_first(in_folder+myfile) )
-    master_ob = merge_obs(obs)
-optics         = kfm.h5_to_dict(myfile, group = 'beam-optics')
-particle_on_CO = kfm.h5_to_dict(myfile, group = 'closed-orbit')
-kfm.dict_to_h5(master_ob, outfile, group='data', readwrite_opts='w')
-kfm.dict_to_h5(optics, outfile, group='optics', readwrite_opts='a')
-kfm.dict_to_h5(particle_on_CO, outfile, group='closed-orbit', readwrite_opts='a')
+suffixes = ['_init.h5', '_first.h5', '_skip.h5', '_last.h5']
+ob_funcs = [get_ob_init, get_ob_first, get_ob_skip, get_ob_last]
+for filelist,fname in zip(filelists, fnames):
+    for suffix, get_ob in zip(suffixes, ob_funcs):
+        fname_suff = fname+suffix
+        print(fname_suff)
+        outfile = out_folder + fname_suff
+        obs = []
+        for myfile in filelist:
+            obs.append( get_ob(in_folder+myfile) )
+        master_ob = merge_obs(obs)
+        optics         = kfm.h5_to_dict(myfile, group = 'beam-optics')
+        particle_on_CO = kfm.h5_to_dict(myfile, group = 'closed-orbit')
+        kfm.dict_to_h5(master_ob, outfile, group='data', readwrite_opts='w')
+        kfm.dict_to_h5(optics, outfile, group='optics', readwrite_opts='a')
+        kfm.dict_to_h5(particle_on_CO, outfile, group='closed-orbit', readwrite_opts='a')
 
 
 
